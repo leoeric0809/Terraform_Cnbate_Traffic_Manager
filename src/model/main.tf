ca0168bf2b09f885073c03d8a710f070ca29e924
Frank Fong <frankfong0208@gmail.com>
2021-02-14 21:56:04 +0000

    update4

--- src/model/main.tf
+++ src/model/main.tf
@@ -6,8 +6,19 @@ terraform {
   }
 }
 
+
+
+terraform {
+  required_version = ">= 0.14"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>2.47.0"
+    }
+  }
+}
+
 provider "azurerm" {
-  version = "~>2.0"
   features {}
 }
 
